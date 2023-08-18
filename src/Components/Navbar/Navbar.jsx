7f457e86cc4f6a2f67c9ca859fc029bbd09192a2
Igor.K <igor.k.nsk@gmail.com>
2023-08-18 17:02:36 +0700

    add resize

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from "react-redux"
 import { Flex, Heading } from '@chakra-ui/react';
 import StyleColorMode from '../StyleColorMode/StyleColorMode';
 
 const Navbar = React.memo(() => {
+
+  const [modalHeight, setModalHeight] = useState(window.innerHeight);
+
   const title = useSelector((store) => store.title)
   useEffect(() => {
     console.log("---Render Navbar");
@@ -12,6 +15,8 @@ const Navbar = React.memo(() => {
     <Flex w='100%' h='3rem' p={'.5rem'} backgroundColor={'custom.navBar.light'} mb={['1rem', '2rem']} justifyContent={'space-between'} alignItems={'center'}>
       <StyleColorMode></StyleColorMode>
       <Heading as='h3' size='md' color={'white'}>
+        <span>{modalHeight}</span>
+        {'  '}
         {title}
       </Heading>
     </Flex>
